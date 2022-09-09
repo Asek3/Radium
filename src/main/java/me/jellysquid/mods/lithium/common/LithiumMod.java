76cac18f8815da63adfed65e4eef7dafb7cab644
NanoLive <bogban.zp@gmail.com>
2022-09-09 15:49:54 +0300

    Ignore mod on another side

--- src/main/java/me/jellysquid/mods/lithium/common/LithiumMod.java
+++ src/main/java/me/jellysquid/mods/lithium/common/LithiumMod.java
@@ -2,8 +2,11 @@ package me.jellysquid.mods.lithium.common;
 
 import me.jellysquid.mods.lithium.common.config.LithiumConfig;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.IExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.network.NetworkConstants;
 
 @Mod(LithiumMod.MODID)
 public class LithiumMod {
@@ -13,6 +16,8 @@ public class LithiumMod {
 
     public LithiumMod() {
         MinecraftForge.EVENT_BUS.addListener(this::onInitialize);
+        
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
     }
     
     public void onInitialize(final FMLCommonSetupEvent event) {
